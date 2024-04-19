d7a221d10a28214089657290495b485afa84d935
theMladyPan <stanislav@rubint.sk>
2024-04-19 22:44:45 +0200

    Fix SPI communication and add temperature reading functionality

--- src/Sensors/Murata/SCL3300.cpp
+++ src/Sensors/Murata/SCL3300.cpp
@@ -5,163 +5,170 @@
 #include "hardware/spi.h"
 #include <sstream>
 #include "Core/Errors.h"
+#include "Utils/Log.h"
 
 namespace Xerxes
 {
 
-
-void SCL3300::initSequence()
-{
-    // chip init sequence    
-    volatile uint32_t received_packet;
-
-    sleep_ms(1);
-    ExchangeBlock(CMD::Switch_to_bank_0);
-    sleep_ms(3);
-
-    ExchangeBlock(CMD::SW_Reset);
-    sleep_ms(3);
-
-    ExchangeBlock(CMD::Change_to_mode_4);
-    sleep_ms(1);
-    ExchangeBlock(CMD::Enable_ANGLE);
-    sleep_ms(100);
-
-    ExchangeBlock(CMD::Read_Status_Summary);
-    ExchangeBlock(CMD::Read_Status_Summary);
-    ExchangeBlock(CMD::Read_Status_Summary);
-    sleep_ms(1);
-    ExchangeBlock(CMD::Read_WHOAMI);
-    ExchangeBlock(CMD::Read_WHOAMI);
-
-    needInit = false;
-}
-
-
-void SCL3300::init()
-{    
-    _devid = DEVID_ANGLE_XY_90;
-
-    constexpr uint spi_freq = 2 * MHZ;
-    // init spi with freq , return actual frequency
-    uint baudrate = spi_init(spi0, spi_freq);
-    spi_set_format(spi0, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-
-    // Set the GPIO pin mux to the SPI
-    gpio_set_function(SPI0_MISO_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI0_MOSI_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI0_CLK_PIN, GPIO_FUNC_SPI);
-
-    gpio_init(EXT_3V3_EN_PIN);
-    gpio_set_dir(EXT_3V3_EN_PIN, GPIO_OUT);
-
-    // enable sensor 3V3
-    gpio_put(EXT_3V3_EN_PIN, true);
-
-    // CS pin as GPIO
-    gpio_init(SPI0_CSN_PIN);
-    gpio_set_dir(SPI0_CSN_PIN, GPIO_OUT);
-
-    // call init sequence
-    initSequence();
-    
-    // change sample rate to 10Hz
-    *_reg->desiredCycleTimeUs = _sensorUpdateRateUs;
-
-    this->update();
-}
-
-
-void SCL3300::update()
-{    
-    // check if init sequence is needed
-    if (needInit)
+    void SCL3300::initSequence()
     {
+        // chip init sequence
+        volatile uint32_t received_packet;
+
+        sleep_ms(1);
+        ExchangeBlock(CMD::Switch_to_bank_0);
+        sleep_ms(3);
+
+        ExchangeBlock(CMD::SW_Reset);
+        sleep_ms(3);
+
+        ExchangeBlock(CMD::Change_to_mode_4);
+        sleep_ms(1);
+        ExchangeBlock(CMD::Enable_ANGLE);
+        sleep_ms(100);
+
+        ExchangeBlock(CMD::Read_Status_Summary);
+        ExchangeBlock(CMD::Read_Status_Summary);
+        ExchangeBlock(CMD::Read_Status_Summary);
+        sleep_ms(1);
+        ExchangeBlock(CMD::Read_WHOAMI);
+        ExchangeBlock(CMD::Read_WHOAMI);
+
+        needInit = false;
+    }
+
+    void SCL3300::init()
+    {
+        _devid = DEVID_ANGLE_XY_90;
+        _label = "Inclinometer SCL3300 +/- 10°";
+
+        constexpr uint spi_freq = 4 * MHZ;
+        // init spi with freq , return actual frequency
+        uint baudrate = spi_init(spi0, spi_freq);
+        spi_set_format(spi0, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+
+        // Set the GPIO pin mux to the SPI
+        gpio_set_function(SPI0_MISO_PIN, GPIO_FUNC_SPI);
+        gpio_set_function(SPI0_MOSI_PIN, GPIO_FUNC_SPI);
+        gpio_set_function(SPI0_CLK_PIN, GPIO_FUNC_SPI);
+
+        gpio_init(EXT_3V3_EN_PIN);
+        gpio_set_dir(EXT_3V3_EN_PIN, GPIO_OUT);
+
+        // enable sensor 3V3
+        gpio_put(EXT_3V3_EN_PIN, true);
+
+        // CS pin as GPIO
+        gpio_init(SPI0_CSN_PIN);
+        gpio_set_dir(SPI0_CSN_PIN, GPIO_OUT);
+
+        // call init sequence
         initSequence();
+
+        // change sample rate to 10Hz
+        *_reg->desiredCycleTimeUs = _sensorUpdateRateUs;
+
+        this->update();
     }
 
-    // declare packet variables
-    auto packetX = std::make_unique<SclPacket_t>();
-    auto packetY = std::make_unique<SclPacket_t>();
-    // auto packetZ = std::make_unique<SclPacket_t>();  // not used anymore
-    auto packetT = std::make_unique<SclPacket_t>();
-    
-    // read sensor data from sensor twice because of communication shift
-    ExchangeBlock(CMD::Read_ANG_X);  // this will read the last value from the previous update
-    longToPacket(ExchangeBlock(CMD::Read_ANG_Y), packetX);
-
-    // longToPacket(ExchangeBlock(CMD::Read_ANG_Z), packetY);
-    longToPacket(ExchangeBlock(CMD::Read_Temperature), packetY);
-    longToPacket(ExchangeBlock(CMD::Read_Status_Summary), packetT);
-
-    // convert data to angles
-    *_reg->pv0 = static_cast<float>(getDegFromPacket(packetX));
-    *_reg->pv1 = static_cast<float>(getDegFromPacket(packetY));
-    // pv2 = static_cast<float>(getDegFromPacket(packetZ));
-
-    if (!packetX->DATA_H && \
-        !packetX->DATA_L && \
-        !packetY->DATA_H && \
-        !packetY->DATA_L)
+    void SCL3300::update()
     {
-        // sensor is working but not giving any data - reinit
-        this->needInit = true;
-    }    
+        // check if init sequence is needed
+        if (needInit)
+        {
+            initSequence();
+        }
 
-    // extract temperature from packet and convert to degrees
-    uint16_t raw_temp = (uint16_t)(packetT->DATA_H << 8) + packetT->DATA_L;
-    *_reg->pv3 = -273 + (static_cast<float>(raw_temp) / 18.9);
+        // declare packet variables
+        auto packetX = std::make_unique<SclPacket_t>();
+        auto packetY = std::make_unique<SclPacket_t>();
+        // auto packetZ = std::make_unique<SclPacket_t>();  // not used anymore
+        auto packetT = std::make_unique<SclPacket_t>();
 
-    // if calcStat is true, update statistics
-    if(_reg->config->bits.calcStat)
-    {
-        // insert new values into ring buffer
-        rbpv0.insertOne(*_reg->pv0);
-        rbpv1.insertOne(*_reg->pv1);
-        rbpv3.insertOne(*_reg->pv3);
+        // read sensor data from sensor twice because of communication shift
+        ExchangeBlock(CMD::Read_ANG_X); // this will read the last value from the previous update
+        longToPacket(ExchangeBlock(CMD::Read_ANG_Y), packetX);
 
-        // update statistics
-        rbpv0.updateStatistics();
-        rbpv1.updateStatistics();
-        rbpv3.updateStatistics();
+        // longToPacket(ExchangeBlock(CMD::Read_ANG_Z), packetY);
+        longToPacket(ExchangeBlock(CMD::Read_Temperature), packetY);
+        longToPacket(ExchangeBlock(CMD::Read_Status_Summary), packetT);
 
-        // update min, max stddev etc...
-        rbpv0.getStatistics(_reg->minPv0, _reg->maxPv0, _reg->meanPv0, _reg->stdDevPv0);
-        rbpv1.getStatistics(_reg->minPv1, _reg->maxPv1, _reg->meanPv1, _reg->stdDevPv1);
-        rbpv3.getStatistics(_reg->minPv3, _reg->maxPv3, _reg->meanPv3, _reg->stdDevPv3);
+        // convert data to angles
+        *_reg->pv0 = static_cast<float>(getDegFromPacket(packetX));
+        *_reg->pv1 = static_cast<float>(getDegFromPacket(packetY));
+        // pv2 = static_cast<float>(getDegFromPacket(packetZ));
+
+        if (!packetX->DATA_H &&
+            !packetX->DATA_L &&
+            !packetY->DATA_H &&
+            !packetY->DATA_L)
+        {
+            // sensor is working but not giving any data - reinit
+            this->needInit = true;
+        }
+
+        // extract temperature from packet and convert to degrees
+        uint16_t raw_temp = (uint16_t)(packetT->DATA_H << 8) + packetT->DATA_L;
+        float temp = -273 + (static_cast<float>(raw_temp) / 18.9);
+        if (temp < -40 || temp > 85)
+        {
+            // temperature is out of range
+            xlog_warn("Temperature out of range: " << temp);
+        }
+        else
+        {
+            *_reg->pv3 = temp;
+        }
+
+        // if calcStat is true, update statistics
+        if (_reg->config->bits.calcStat)
+        {
+            // insert new values into ring buffer
+            rbpv0.insertOne(*_reg->pv0);
+            rbpv1.insertOne(*_reg->pv1);
+            rbpv3.insertOne(*_reg->pv3);
+
+            // update statistics
+            rbpv0.updateStatistics();
+            rbpv1.updateStatistics();
+            rbpv3.updateStatistics();
+
+            // update min, max stddev etc...
+            rbpv0.getStatistics(_reg->minPv0, _reg->maxPv0, _reg->meanPv0, _reg->stdDevPv0);
+            rbpv1.getStatistics(_reg->minPv1, _reg->maxPv1, _reg->meanPv1, _reg->stdDevPv1);
+            rbpv3.getStatistics(_reg->minPv3, _reg->maxPv3, _reg->meanPv3, _reg->stdDevPv3);
+        }
     }
-}
 
+    double SCL3300::getDegFromPacket(const std::unique_ptr<SclPacket_t> &packet)
+    {
+        // convert to signed int from 2's complement representation
+        int16_t raw = (int16_t)(packet->DATA_H << 8) + (int16_t)packet->DATA_L;
 
-double SCL3300::getDegFromPacket(const std::unique_ptr<SclPacket_t>& packet)
-{
-    // convert to signed int from 2's complement representation
-    int16_t raw = (int16_t)(packet->DATA_H << 8) + (int16_t)packet->DATA_L;
-    
-    double degrees = (double)raw * 180 / (1 << 15);
-    return degrees;
-}
+        double degrees = (double)raw * 180 / (1 << 15);
+        return degrees;
+    }
 
+    std::string SCL3300::getJson()
+    {
+        using namespace std;
 
-std::string SCL3300::getJson()
-{
-    using namespace std;
+        stringstream ss;
 
-    stringstream ss;
+        // return values as JSON
+        ss << "{" << endl;
+        ss << "\t\"X\":" << *_reg->pv0 << "," << endl;
+        ss << "\t\"Y\":" << *_reg->pv1 << "," << endl;
+        ss << "\t\"t\":" << *_reg->pv3 << "," << endl;
+        ss << "\t\"Avg(X)\":" << *_reg->meanPv0 << "," << endl;
+        ss << "\t\"Avg(Y)\":" << *_reg->meanPv1 << "," << endl;
+        ss << "\t\"StdDev(X)\":" << *_reg->stdDevPv0 << "," << endl;
+        ss << "\t\"StdDev(Y)\":" << *_reg->stdDevPv1 << "," << endl;
+        ss << "\t\"Avg(t)\":" << *_reg->meanPv3 << "," << endl;
 
-    // return values as JSON
-    ss << "{" << endl;
-    ss << "\t\"X\":" << *_reg->pv0 << "," << endl;
-    ss << "\t\"Y\":" << *_reg->pv1 << "," << endl;
-    ss << "\t\"Avg(X)\":" << *_reg->meanPv0 << "," << endl;
-    ss << "\t\"Avg(Y)\":" << *_reg->meanPv1 << "," << endl;
-    ss << "\t\"StdDev(X)\":" << *_reg->stdDevPv0 << "," << endl;
-    ss << "\t\"StdDev(Y)\":" << *_reg->stdDevPv1 << "," << endl;
-    ss << "\t\"Avg(t)\":" << *_reg->meanPv3 << "," << endl;
+        ss << "}";
 
-    ss << "}";
-
-    return ss.str();
-}
+        return ss.str();
+    }
 
 } // namespace Xerxes
--- src/Sensors/Murata/SCL3X00.cpp
+++ src/Sensors/Murata/SCL3X00.cpp
@@ -7,139 +7,126 @@
 #include "Core/Errors.h"
 #include "Core/Register.hpp"
 
-
 namespace Xerxes
 {
 
-
-uint32_t SCL3X00::ExchangeBlock(const uint32_t &block)
-{
-    uint32_t rcvd;
-    // SPI_CS active low, read data
-    gpio_put(SPI0_CSN_PIN, 0);
-    uint32_t reordered = __builtin_bswap32(block);
-
-    sleep_us(5);
-
-    // exchange data
-    spi_write_read_blocking(spi0, (uint8_t*)&reordered, (uint8_t*)&rcvd, 4);
-
-    // release CS
-    gpio_put(SPI0_CSN_PIN, 1);
-
-    rcvd = __builtin_bswap32(rcvd);
-
-    // check if the rcvd data is all zeroes or all 1 - communication error
-    if (rcvd == 0xFFFFFFFF || rcvd == 0x00000000)
+    uint32_t SCL3X00::ExchangeBlock(const uint32_t &block)
     {
-        // set error flag - sensor is not connected
-        _reg->errorSet(ERROR_MASK_SENSOR_CONNECTION);
+        uint32_t rcvd;
+        // SPI_CS active low, read data
+        gpio_put(SPI0_CSN_PIN, 0);
+        uint32_t reordered = __builtin_bswap32(block);
 
-        // set error flag - sensor was not connnected in past
-        _reg->errorSet(ERROR_MASK_SENSOR_CONNECTION_MEM);  // set error flag
+        sleep_us(10);
+
+        // exchange data
+        spi_write_read_blocking(spi0, (uint8_t *)&reordered, (uint8_t *)&rcvd, 4);
+
+        // release CS
+        gpio_put(SPI0_CSN_PIN, 1);
+        sleep_us(5);
+
+        rcvd = __builtin_bswap32(rcvd);
+
+        // check if the rcvd data is all zeroes or all 1 - communication error
+        if (rcvd == 0xFFFFFFFF || rcvd == 0x00000000)
+        {
+            // set error flag - sensor is not connected
+            _reg->errorSet(ERROR_MASK_SENSOR_CONNECTION);
+
+            // set error flag - sensor was not connnected in past
+            _reg->errorSet(ERROR_MASK_SENSOR_CONNECTION_MEM); // set error flag
+        }
+        else
+        {
+            // clear error flag - sensor is connected now
+            _reg->errorClear(ERROR_MASK_SENSOR_CONNECTION);
+        }
+
+        return rcvd;
     }
-    else
+
+    void SCL3X00::stop()
     {
-        // clear error flag - sensor is connected now
-        _reg->errorClear(ERROR_MASK_SENSOR_CONNECTION);
+        gpio_put(EXT_3V3_EN_PIN, false);
+        spi_deinit(spi0);
+        gpio_set_dir(EXT_3V3_EN_PIN, GPIO_IN);
+        gpio_set_dir(SPI0_CSN_PIN, GPIO_IN);
     }
 
-    sleep_us(5);
-    
-    return rcvd;
-}
-
-
-void SCL3X00::stop()
-{
-    gpio_put(EXT_3V3_EN_PIN, false);
-    spi_deinit(spi0);
-    gpio_set_dir(EXT_3V3_EN_PIN, GPIO_IN);
-    gpio_set_dir(SPI0_CSN_PIN, GPIO_IN);
-
-}
-
-
-void SCL3X00::longToPacket(const uint32_t data, SclPacket_t *packet)
-{
-    packet->RW        = static_cast<uint8_t>(data >> 31);
-    packet->ADDR      = static_cast<uint8_t>(data >> 26);
-    packet->RS        = static_cast<uint8_t>(data >> 24);
-    packet->DATA_H    = static_cast<uint8_t>(data >> 16);
-    packet->DATA_L    = static_cast<uint8_t>(data >> 8);
-    packet->CRC       = static_cast<uint8_t>(data);
-}
-
-
-void SCL3X00::longToPacket(const uint32_t data, std::unique_ptr<SclPacket_t>& packet)
-{
-    packet->RW        = static_cast<uint8_t>(data >> 31);
-    packet->ADDR      = static_cast<uint8_t>(data >> 26);
-    packet->RS        = static_cast<uint8_t>(data >> 24);
-    packet->DATA_H    = static_cast<uint8_t>(data >> 16);
-    packet->DATA_L    = static_cast<uint8_t>(data >> 8);
-    packet->CRC       = static_cast<uint8_t>(data);
-}
-
-
-double SCL3X00::SclReadTemp()
-{
-    auto packet = std::make_unique<SclPacket_t>();
-
-    longToPacket(ExchangeBlock(CMD::Read_Temperature), packet);
-    longToPacket(ExchangeBlock(CMD::Read_Temperature), packet);
-
-
-    uint16_t raw_temp = (uint16_t)(packet->DATA_H << 8) + packet->DATA_L;
-    double degrees = -273 + ((double)raw_temp / 18.9);
-    return degrees;
-}
-
-
-scl_status_t SCL3X00::SclStatus()
-{
-    auto status = std::make_unique<SclPacket_t>();
-    longToPacket(ExchangeBlock(CMD::Read_Status_Summary), status);
-    longToPacket(ExchangeBlock(CMD::Read_Status_Summary), status);
-    
-    // RS: '01' - Normal operation, no flags
-    return static_cast<scl_status_t>(status->RS);
-}
-
-
-// Calculate CRC for 24 MSB's of the 32 bit dword
-// (8 LSB's are the CRC field and are not included in CRC calculation)
-uint8_t CalculateCRC(uint32_t Data)
-{
-    uint8_t BitIndex;
-    uint8_t BitValue;
-    uint8_t CRC;
-    CRC = 0xFF;
-    for (BitIndex = 31; BitIndex > 7; BitIndex--)
+    void SCL3X00::longToPacket(const uint32_t data, SclPacket_t *packet)
     {
-        BitValue = (uint8_t)((Data >> BitIndex) & 0x01);
-        CRC = CRC8(BitValue, CRC);
+        packet->RW = static_cast<uint8_t>(data >> 31);
+        packet->ADDR = static_cast<uint8_t>(data >> 26);
+        packet->RS = static_cast<uint8_t>(data >> 24);
+        packet->DATA_H = static_cast<uint8_t>(data >> 16);
+        packet->DATA_L = static_cast<uint8_t>(data >> 8);
+        packet->CRC = static_cast<uint8_t>(data);
     }
-    CRC = (uint8_t)~CRC;
-    return CRC;
-}
 
-
-uint8_t CRC8(uint8_t BitValue, uint8_t CRC)
-{
-    uint8_t Temp;
-    Temp = (uint8_t)(CRC & 0x80);
-    if (BitValue == 0x01)
+    void SCL3X00::longToPacket(const uint32_t data, std::unique_ptr<SclPacket_t> &packet)
     {
-        Temp ^= 0x80;
+        packet->RW = static_cast<uint8_t>(data >> 31);
+        packet->ADDR = static_cast<uint8_t>(data >> 26);
+        packet->RS = static_cast<uint8_t>(data >> 24);
+        packet->DATA_H = static_cast<uint8_t>(data >> 16);
+        packet->DATA_L = static_cast<uint8_t>(data >> 8);
+        packet->CRC = static_cast<uint8_t>(data);
     }
-    CRC <<= 1;
-    if (Temp > 0)
-    {
-        CRC ^= 0x1D;
-    }
-    return CRC;
-}
 
+    double SCL3X00::SclReadTemp()
+    {
+        auto packet = std::make_unique<SclPacket_t>();
+
+        longToPacket(ExchangeBlock(CMD::Read_Temperature), packet);
+        longToPacket(ExchangeBlock(CMD::Read_Temperature), packet);
+
+        uint16_t raw_temp = (uint16_t)(packet->DATA_H << 8) + packet->DATA_L;
+        double degrees = -273 + ((double)raw_temp / 18.9);
+        return degrees;
+    }
+
+    scl_status_t SCL3X00::SclStatus()
+    {
+        auto status = std::make_unique<SclPacket_t>();
+        longToPacket(ExchangeBlock(CMD::Read_Status_Summary), status);
+        longToPacket(ExchangeBlock(CMD::Read_Status_Summary), status);
+
+        // RS: '01' - Normal operation, no flags
+        return static_cast<scl_status_t>(status->RS);
+    }
+
+    // Calculate CRC for 24 MSB's of the 32 bit dword
+    // (8 LSB's are the CRC field and are not included in CRC calculation)
+    uint8_t CalculateCRC(uint32_t Data)
+    {
+        uint8_t BitIndex;
+        uint8_t BitValue;
+        uint8_t CRC;
+        CRC = 0xFF;
+        for (BitIndex = 31; BitIndex > 7; BitIndex--)
+        {
+            BitValue = (uint8_t)((Data >> BitIndex) & 0x01);
+            CRC = CRC8(BitValue, CRC);
+        }
+        CRC = (uint8_t)~CRC;
+        return CRC;
+    }
+
+    uint8_t CRC8(uint8_t BitValue, uint8_t CRC)
+    {
+        uint8_t Temp;
+        Temp = (uint8_t)(CRC & 0x80);
+        if (BitValue == 0x01)
+        {
+            Temp ^= 0x80;
+        }
+        CRC <<= 1;
+        if (Temp > 0)
+        {
+            CRC ^= 0x1D;
+        }
+        return CRC;
+    }
 
 }
