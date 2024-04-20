6bf9effe4ea68a39d1904552ec569b12a1107cb6
theMladyPan <stanislav@rubint.sk>
2024-04-20 22:52:53 +0200

    successfuly reading data from sensor, XYZ and temp

--- src/Sensors/ST/Lis2dw12.cpp
+++ src/Sensors/ST/Lis2dw12.cpp
@@ -7,11 +7,18 @@ namespace Xerxes
 {
     namespace CTRL1
     {
-        uint8_t val(ODR_t odr, MODE_t mode, LP_MODE_t lp_mode)
+        uint8_t val(ODR odr, MODE mode, LP_MODE lp_mode)
         {
             // Shift each enum to its position and combine using bitwise OR
             return ((uint8_t)odr << 4) | ((uint8_t)mode << 2) | (uint8_t)lp_mode;
         }
+    }
+
+    const bool LIS2::dataReady() const
+    {
+        Status_t status;
+        status.reg = readRegister(REG::STATUS);
+        return status.bits.DRDY;
     }
 
     void LIS2::init()
@@ -38,10 +45,22 @@ namespace Xerxes
         sleep_ms(20);
         if (whoami() != 0x44)
         {
-            xlog_error("LIS2 init failed");
-            // restart
+            xlog_error("LIS2 whoami failed");
             throw std::runtime_error("LIS2 init failed");
         }
+
+        // set CTRL1 register
+        writeRegister(REG::CTRL1, CTRL1::val(CTRL1::ODR::ODR_25HZ, CTRL1::MODE::HIGH_PERF, CTRL1::LP_MODE::LP_MODE1));
+        uint8_t ctrl1 = readRegister(REG::CTRL1);
+        xlog_debug("LIS2 CTRL1: " << (int)ctrl1);
+
+        writeRegister(REG::CTRL6, CTRL6::BW_FILT_ODR_2 | CTRL6::FS_2G | CTRL6::FDS_LOW_PASS | CTRL6::LOW_NOISE_ENABLED);
+        uint8_t ctrl6 = readRegister(REG::CTRL6);
+        xlog_debug("LIS2 CTRL6: " << (int)ctrl6);
+
+        Status_t status;
+        status.reg = readRegister(REG::STATUS);
+        xlog_debug("LIS2 STATUS: " << (int)status.reg);
     }
 
     void LIS2::stop()
@@ -50,13 +69,43 @@ namespace Xerxes
         gpio_set_dir(SPI0_CSN_PIN, GPIO_IN);
     }
 
-    void LIS2::update() {}
+    void LIS2::update()
+    {
+        while (!dataReady())
+        {
+            sleep_us(1);
+        }
+        uint8_t x_l = readRegister(REG::OUT_X_L);
+        uint8_t x_h = readRegister(REG::OUT_X_H);
+        uint8_t y_l = readRegister(REG::OUT_Y_L);
+        uint8_t y_h = readRegister(REG::OUT_Y_H);
+        uint8_t z_l = readRegister(REG::OUT_Z_L);
+        uint8_t z_h = readRegister(REG::OUT_Z_H);
+        uint8_t t_l = readRegister(REG::OUT_T_L);
+        uint8_t t_h = readRegister(REG::OUT_T_H);
+        int16_t x = (int16_t)(x_h << 8 | x_l);
+        int16_t y = (int16_t)(y_h << 8 | y_l);
+        int16_t z = (int16_t)(z_h << 8 | z_l);
+        int16_t t = (int16_t)(t_h << 4 | t_l >> 4);
+        *_reg->pv0 = (float)x * 2 / (1 << 15);
+        *_reg->pv1 = (float)y * 2 / (1 << 15);
+        *_reg->pv2 = (float)z * 2 / (1 << 15);
+        *_reg->pv3 = ((float)t / 16.0) + 25;
+        std::cout << "[" << time_us_64() << "] x: " << *_reg->pv0 << ", y: " << *_reg->pv1 << ", z: " << *_reg->pv2 << ", t: " << *_reg->pv3 << std::endl;
+        float len = sqrtf(*_reg->pv0 * *_reg->pv0 + *_reg->pv1 * *_reg->pv1 + *_reg->pv2 * *_reg->pv2);
+        std::cout << "len: " << len << std::endl;
+
+        // super::update();
+    }
 
     std::string LIS2::getJson()
     {
         std::stringstream ss;
         ss << "{";
-        ss << "whoami: " << whoami();
+        ss << "\"x\":" << *_reg->pv0 << ",";
+        ss << "\"y\":" << *_reg->pv1 << ",";
+        ss << "\"z\":" << *_reg->pv2 << ",";
+        ss << "\"t\":" << *_reg->pv3;
         ss << "}" << std::endl;
         return ss.str();
     }
@@ -69,7 +118,7 @@ namespace Xerxes
     const uint8_t LIS2::readRegister(uint8_t reg) const
     {
         gpio_put(SPI0_CSN_PIN, 0);
-        sleep_us(10);
+        sleep_us(1);
         // send read command
         const uint8_t _cmd = reg | READ;
         spi_write_blocking(spi0, &_cmd, 1);
@@ -83,8 +132,31 @@ namespace Xerxes
         {
             xlog_error("LIS2 readRegister failed");
         }
-        xlog_debug("LIS2 readRegister: " << std::hex << (int)data << std::dec);
+        xlog_debug("LIS2 readRegister: 0x" << std::hex << (int)reg << ", val: " << (int)data << std::dec);
 
         return data;
+    }
+
+    const bool LIS2::writeRegister(uint8_t reg, uint8_t val) const
+    {
+        gpio_put(SPI0_CSN_PIN, 0);
+        sleep_us(10);
+        // send write command
+        const uint8_t _cmd = reg | WRITE;
+        spi_write_blocking(spi0, &_cmd, 1);
+
+        // write data
+        const uint8_t _l = spi_write_blocking(spi0, &val, 1);
+        gpio_put(SPI0_CSN_PIN, 1);
+
+        if (_l != 1)
+        {
+            xlog_error("LIS2 writeRegister failed");
+            return false;
+        }
+
+        xlog_debug("LIS2 writeRegister: 0x" << std::hex << (int)reg << ", val: " << (int)val << std::dec);
+
+        return true;
     }
 } // namespace Xerxes
--- src/Sensors/ST/Lis2dw12.hpp
+++ src/Sensors/ST/Lis2dw12.hpp
@@ -9,7 +9,7 @@ namespace Xerxes
     namespace CTRL1
     {
         // Enum for ODR (Output Data Rate)
-        typedef enum
+        enum class ODR : uint8_t
         {
             PWR_DOWN = 0x00,       // 0000
             ODR_12_6_1_6HZ = 0x01, // 0001 12.5Hz in High Performance mode, 1.6Hz in Low Power mode
@@ -21,52 +21,88 @@ namespace Xerxes
             ODR_400_200HZ = 0x07,  // 0111 400Hz in High Performance mode, 200Hz in Low Power mode
             ODR_800_200HZ = 0x08,  // 1000 800Hz in High Performance mode, 200Hz in Low Power mode
             ODR_1600_200HZ = 0x09, // 1001 1600Hz in High Performance mode, 200Hz in Low Power mode
-        } ODR_t;
+        };
 
         // Enum for MODE
-        typedef enum
+        enum class MODE : uint8_t
         {
             LOW_PWR = 0x00,   // 00 Low Power Mode (12/14-bit resolution)
             HIGH_PERF = 0x01, // 01 High Performance Mode (14-bit resolution)
             ON_DEMAND = 0x02, // 10 On Demand Mode - single measurement
             // 0x03 reserved
-        } MODE_t;
+        };
 
         // Enum for LP_MODE (Low Power Mode)
-        typedef enum
+        enum class LP_MODE : uint8_t
         {
             LP_MODE1 = 0x00, // 00 Low Power Mode 1 /12-bit resolution
             LP_MODE2 = 0x01, // 01 Low Power Mode 2 /14-bit resolution
             LP_MODE3 = 0x02, // 10 Low Power Mode 3 /14-bit resolution
             LP_MODE4 = 0x03  // 11 Low Power Mode 4 /14-bit resolution
-        } LP_MODE_t;
+        };
 
         // Function to combine enums into CTRL1 (uint8_t)
-        uint8_t val(ODR_t odr, MODE_t mode, LP_MODE_t lp_mode);
+        uint8_t val(ODR odr, MODE mode, LP_MODE lp_mode);
 
     } // namespace CTRL1
 
     namespace CTRL2
     {
-        constexpr uint8_t BOOT = 0x80;        // Boot memory content (0: disabled; 1: enabled), this bit is self-cleared when the BOOT is completed
-        constexpr uint8_t SOFT_RESET = 0x40;  // Soft reset acts as reset for all control registers, then goes to 0.
-        constexpr uint8_t CS_PU_DISC = 0x20;  // Disconnect CS pull-up, 0: pull-up connected, 1: pull-up disconnected
-        constexpr uint8_t BDU = 0x10;         // Block Data Update, 0: continuous update, 1: output registers not updated until MSB and LSB reading
+        constexpr uint8_t BOOT = 0x40;        // Boot memory content (0: disabled; 1: enabled), this bit is self-cleared when the BOOT is completed
+        constexpr uint8_t SOFT_RESET = 0x20;  // Soft reset acts as reset for all control registers, then goes to 0.
+        constexpr uint8_t CS_PU_DISC = 0x10;  // Disconnect CS pull-up, 0: pull-up connected, 1: pull-up disconnected
+        constexpr uint8_t BDU = 0x08;         // Block Data Update, 0: continuous update, 1: output registers not updated until MSB and LSB reading
         constexpr uint8_t IF_ADD_INC = 0x04;  // Register address automatically incremented during a multiple byte access with a serial interface (I2C or SPI) 0: disabled, 1: enabled
         constexpr uint8_t I2C_DISABLE = 0x02; // Disable I2C interface, 0: I2C enabled, 1: I2C disabled
         constexpr uint8_t SIM = 0x01;         // SPI Serial Interface Mode selection, 0: 4-wire interface, 1: 3-wire interface, default is 0: 4-wire interface
 
-        typedef struct
+        typedef struct // TODO: verify order with datasheet
         {
-            bool BOOT : 1;        // Boot memory content (0: disabled; 1: enabled), this bit is self-cleared when the BOOT is completed
-            bool SOFT_RESET : 1;  // Soft reset acts as reset for all control registers, then goes to 0.
-            bool CS_PU_DISC : 1;  // Disconnect CS pull-up, 0: pull-up connected, 1: pull-up disconnected
-            bool BDU : 1;         // Block Data Update, 0: continuous update, 1: output registers not updated until MSB and LSB reading
-            bool IF_ADD_INC : 1;  // Register address automatically incremented during a multiple byte access with a serial interface (I2C or SPI) 0: disabled, 1: enabled
-            bool I2C_DISABLE : 1; // Disable I2C interface, 0: I2C enabled, 1: I2C disabled
-            bool SIM : 1;         // SPI Serial Interface Mode selection, 0: 4-wire interface, 1: 3-wire interface, default is 0: 4-wire interface
+            uint8_t SIM : 1;         // SPI Serial Interface Mode selection, 0: 4-wire interface, 1: 3-wire interface, default is 0: 4-wire interface
+            uint8_t I2C_DISABLE : 1; // Disable I2C interface, 0: I2C enabled, 1: I2C disabled
+            uint8_t IF_ADD_INC : 1;  // Register address automatically incremented during a multiple byte access with a serial interface (I2C or SPI) 0: disabled, 1: enabled
+            uint8_t BDU : 1;         // Block Data Update, 0: continuous update, 1: output registers not updated until MSB and LSB reading
+            uint8_t CS_PU_DISC : 1;  // Disconnect CS pull-up, 0: pull-up connected, 1: pull-up disconnected
+            uint8_t SOFT_RESET : 1;  // Soft reset acts as reset for all control registers, then goes to 0.
+            uint8_t BOOT : 1;        // Boot memory content (0: disabled; 1: enabled), this bit is self-cleared when the BOOT is completed
         } CTRL2_t;
     } // namespace CTRL2
+
+    namespace CTRL6
+    {
+        constexpr uint8_t BW_FILT_ODR_2 = 0x00 << 4; // Bandwidth selection, 00: ODR/2, 01: ODR/4, 10: ODR/10, 11: ODR/20
+        constexpr uint8_t BW_FILT_ODR_4 = 0x01 << 4;
+        constexpr uint8_t BW_FILT_ODR_10 = 0x02 << 4;
+        constexpr uint8_t BW_FILT_ODR_20 = 0x03 << 4;
+
+        constexpr uint8_t FS_2G = 0x00 << 2;
+        constexpr uint8_t FS_4G = 0x01 << 2;
+        constexpr uint8_t FS_8G = 0x02 << 2;
+        constexpr uint8_t FS_16G = 0x03 << 2;
+
+        constexpr uint8_t FDS_LOW_PASS = 0x00 << 1;
+        constexpr uint8_t FDS_HIGH_PASS = 0x01 << 1;
+
+        constexpr uint8_t LOW_NOISE_DISABLED = 0x00;
+        constexpr uint8_t LOW_NOISE_ENABLED = 0x01;
+
+    } // namespace CTRL6
+
+    typedef union
+    {
+        struct // LSB FIRST
+        {
+            uint8_t DRDY : 1;        // Data-ready status, 0: no data is available, 1: a new set of data is available
+            uint8_t FF_IA : 1;       // Free-fall event status, 0: no free-fall event detected, 1: free-fall event detected
+            uint8_t D6D_IA : 1;      // 6D event detection status, 0: no event detected, 1: change in position detected
+            uint8_t SINGLE_TAP : 1;  // Single-tap event status, 0: no tap event detected, 1: tap event detected
+            uint8_t DOUBLE_TAP : 1;  // Double-tap event status, 0: no tap event detected, 1: tap event detected
+            uint8_t SLEEP_STATE : 1; // Sleep event status, 0: no sleep event detected, 1: sleep event detected
+            uint8_t WU_IA : 1;       // Wake-up event detection status, 0: no wake-up event detected, 1: wake-up event detected
+            uint8_t FIFO_THS : 1;    // FIFO threshold status flag, 0: FIFO filling is lower than the threshold, 1: FIFO filling is equal to or higher than the threshold
+        } bits;
+        uint8_t reg;
+    } Status_t;
 
     namespace REG
     {
@@ -107,6 +143,10 @@ namespace Xerxes
 
         /// @brief Read register and return value
         const uint8_t readRegister(uint8_t reg) const;
+
+        const bool writeRegister(uint8_t reg, uint8_t val) const;
+
+        const bool dataReady() const;
 
     protected:
         // typedef Sensor as super class for easier access
