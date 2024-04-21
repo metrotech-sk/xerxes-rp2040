193227d3ac014aa815136e16ebb1b308d6e65910
theMladyPan <stanislav@rubint.sk>
2024-04-21 15:38:15 +0200

    Update project dependencies and settings

--- src/Communication/RS485.cpp
+++ src/Communication/RS485.cpp
@@ -1,145 +1,141 @@
 #include "RS485.hpp"
 #include "Utils/Log.h"
-
+#include "Utils/Gpio.hpp"
 
 namespace Xerxes
 {
 
-
-RS485::RS485(queue_t *queueTx, queue_t *queueRx) : qtx(queueTx), qrx(queueRx)
-{
-}
-
-
-RS485::~RS485()
-{
-}
-
-
-bool RS485::sendData(const Packet & toSend) const
-{
-    // FIXME: this can check if queue has enough space before trying to send data
-    uint16_t sent {0};
-    for(const auto &el:toSend.getData())
+    RS485::RS485(queue_t *queueTx, queue_t *queueRx) : qtx(queueTx), qrx(queueRx)
     {
-        // try to add byte to the queue, if it fails, break the loop
-        if(!queue_try_add(qtx, &el)) break;
-        sent++; // byte was sent successfully
     }
 
-    
-    // return true if whole packet was sent
-    return sent == toSend.size();
-}
-
-
-bool RS485::readData(const uint64_t timeoutUs, Packet &packet)
-{
-    // start timer
-    uint64_t start = time_us_64();
-
-    // if RX queue is empty, immediately return
-    if(queue_is_empty(qrx))
+    RS485::~RS485()
     {
+    }
+
+    bool RS485::sendData(const Packet &toSend) const
+    {
+        // FIXME: this can check if queue has enough space before trying to send data
+        uint16_t sent{0};
+        for (const auto &el : toSend.getData())
+        {
+            // try to add byte to the queue, if it fails, break the loop
+            if (!queue_try_add(qtx, &el))
+                break;
+            sent++; // byte was sent successfully
+        }
+
+        // return true if whole packet was sent
+        return sent == toSend.size();
+    }
+
+    bool RS485::readData(const uint64_t timeoutUs, Packet &packet)
+    {
+        // start timer
+        uint64_t start = time_us_64();
+
+        // if RX queue is empty, immediately return
+        if (queue_is_empty(qrx))
+        {
+            return false;
+        }
+
+        if (receivePacket(timeoutUs))
+        {
+            ledBusyOn();
+            packet = Packet(incomingMessage);
+            ledBusyO ff();
+
+            return true;
+        }
+
         return false;
     }
 
-    if(receivePacket(timeoutUs))
+    bool RS485::receivePacket(const uint64_t timeoutUs)
     {
-        packet = Packet(incomingMessage);
-        return true;
-    }
+        // check if the packet is in fifo buffer
+        uint8_t nextVal = 0;
 
-    return false;
-}
+#ifdef NDEBUG
+        uint64_t tout = time_us_64() + timeoutUs;
+#else
+        absolute_time_t tout;
+        tout._private_us_since_boot = time_us_64() + timeoutUs;
+#endif // NDEBUG
 
+        uint8_t chks = Xerxes::SOH;
 
-bool RS485::receivePacket(const uint64_t timeoutUs)
-{
-    // check if the packet is in fifo buffer
-    uint8_t nextVal = 0;
-    
-    #ifdef NDEBUG
-    uint64_t tout = time_us_64() + timeoutUs;
-    #else
-    absolute_time_t tout;
-    tout._private_us_since_boot = time_us_64() + timeoutUs;
-    #endif // NDEBUG
+        uint8_t msgLen = 0;
+        volatile bool waitForSoh = true;
 
-    uint8_t chks = Xerxes::SOH;
+        // clear buffer
+        incomingMessage.clear();
 
-    uint8_t msgLen = 0;
-    volatile bool waitForSoh = true;
-
-    // clear buffer
-    incomingMessage.clear();
-
-    while(!time_reached(tout))
-    {
-        if(waitForSoh)
+        while (!time_reached(tout))
         {
-            if(queue_try_remove(qrx, &nextVal))
+            if (waitForSoh)
             {
-                if(nextVal == Xerxes::SOH)
+                if (queue_try_remove(qrx, &nextVal))
                 {
-                    waitForSoh = false;
+                    if (nextVal == Xerxes::SOH)
+                    {
+                        waitForSoh = false;
+                    }
+                }
+            }
+            else
+            {
+                // SOH was found, get msgLen
+                if (queue_try_remove(qrx, &msgLen))
+                {
+                    // add msglen to checksum
+                    chks += msgLen;
+                    break; // break from this loop, continue to receive all data
                 }
             }
         }
-        else
+
+        msgLen -= 3; // SOH and len was received, checksum is received at the end hence -3
+        while (!time_reached(tout) && msgLen)
         {
-            // SOH was found, get msgLen
-            if(queue_try_remove(qrx, &msgLen))
+            if (queue_try_remove(qrx, &nextVal))
             {
-                // add msglen to checksum
-                chks += msgLen;
-                break; //break from this loop, continue to receive all data
+                // something was received, slap it to the incoming vector
+                incomingMessage.emplace_back(nextVal);
+                chks += nextVal;
+                msgLen--;
             }
         }
-    }
 
-    msgLen -= 3; // SOH and len was received, checksum is received at the end hence -3
-    while(!time_reached(tout) && msgLen)
-    {
-        if(queue_try_remove(qrx, &nextVal))
+        while (!time_reached(tout))
         {
-            // something was received, slap it to the incoming vector
-            incomingMessage.emplace_back(nextVal);
-            chks += nextVal;
-            msgLen--;
-        }
-    }
-
-    while(!time_reached(tout))
-    {
-        //wait for checksum byte
-        if(queue_try_remove(qrx, &nextVal))
-        {
-            chks += nextVal;
-            if(chks == 0)
+            // wait for checksum byte
+            if (queue_try_remove(qrx, &nextVal))
             {
-                // successfully received whole message
-                xlog_dbg("Successfully received packet: " << incomingMessage.size() << " bytes");
-                return true;
-            }   
-            else
-            {
-                xlog_warn("Checksum error, expected 0, got " << std::hex << chks);
-                return false;
+                chks += nextVal;
+                if (chks == 0)
+                {
+                    // successfully received whole message
+                    xlog_dbg("Successfully received packet: " << incomingMessage.size() << " bytes");
+                    return true;
+                }
+                else
+                {
+                    xlog_warn("Checksum error, expected 0, got " << std::hex << chks << std::dec);
+                    return false;
+                }
             }
         }
+
+        xlog_warn("Timeout while receiving packet");
+        return false;
     }
 
-    xlog_warn("Timeout while receiving packet");
-    return false;
-}
-
-
-uint32_t remainingTime(const uint64_t & start, const uint64_t &timeout)
-{
-    auto current_time = time_us_64();
-    return timeout - (current_time - start);
-}
-
+    uint32_t remainingTime(const uint64_t &start, const uint64_t &timeout)
+    {
+        auto current_time = time_us_64();
+        return timeout - (current_time - start);
+    }
 
 } // namespace Xerxes
--- src/Core/Slave.cpp
+++ src/Core/Slave.cpp
@@ -1,72 +1,63 @@
 #include "Slave.hpp"
 #include "Utils/Log.h"
 
-
 namespace Xerxes
 {
 
-
-Slave::Slave()
-{
-}
-
-
-Slave::Slave(Protocol *protocol, const uint8_t address) : xp(protocol), address(address)
-{
-}
-
-
-Slave::~Slave()
-{
-}
-
-
-void Slave::bind(const msgid_t msgId, std::function<void(const Message&)> _f)
-{
-    bindings.emplace(msgId, _f);
-}
-
-
-void Slave::call(const Message &msg) 
-{
-    if(bindings.contains(msg.msgId)){
-        // call a function bound to messageId
-        bindings[msg.msgId](msg);
-    }
-}
-
-
-bool Slave::send(const uint8_t destinationAddress, const msgid_t msgId)
-{
-    Message message(address, destinationAddress, msgId);
-    return xp->sendMessage(message);
-}
-
-
-bool Slave::send(const uint8_t destinationAddress, const msgid_t msgId, const std::vector<uint8_t> &payload)
-{
-    Message message(address, destinationAddress, msgId, payload);
-    return xp->sendMessage(message);
-}
-
-
-bool Slave::sync(uint32_t timeoutUs)
-{
-    // check for incoming message
-
-    Message incoming = Message();
-    if(!xp->readMessage(incoming, timeoutUs))
+    Slave::Slave()
     {
-        // if no message is in buffer
-        xlog_info("No message in buffer");
-        return false;
     }
-    
-    // call appropriate function
-    xlog_dbg("Calling function for message, msgid: "<< std::hex << incoming.msgId);
-    call(incoming);
-    return true;
-}
 
+    Slave::Slave(Protocol *protocol, const uint8_t address) : xp(protocol), address(address)
+    {
+    }
+
+    Slave::~Slave()
+    {
+    }
+
+    void Slave::bind(const msgid_t msgId, std::function<void(const Message &)> _f)
+    {
+        bindings.emplace(msgId, _f);
+    }
+
+    void Slave::call(const Message &msg)
+    {
+        if (bindings.contains(msg.msgId))
+        {
+            // call a function bound to messageId
+            bindings[msg.msgId](msg);
+        }
+    }
+
+    bool Slave::send(const uint8_t destinationAddress, const msgid_t msgId)
+    {
+        Message message(address, destinationAddress, msgId);
+        return xp->sendMessage(message);
+    }
+
+    bool Slave::send(const uint8_t destinationAddress, const msgid_t msgId, const std::vector<uint8_t> &payload)
+    {
+        Message message(address, destinationAddress, msgId, payload);
+        return xp->sendMessage(message);
+    }
+
+    bool Slave::sync(uint32_t timeoutUs)
+    {
+        // check for incoming message
+
+        Message incoming = Message();
+        if (!xp->readMessage(incoming, timeoutUs))
+        {
+            // if no message is in buffer
+            xlog_dbg("No message in buffer");
+            return false;
+        }
+
+        // call appropriate function
+        xlog_dbg("Calling function for message, msgid: " << std::hex << incoming.msgId << std::dec);
+        call(incoming);
+        return true;
+    }
 
 } // namespace Xerxes
