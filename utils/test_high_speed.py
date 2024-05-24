71d37dbdf4a4942d2719f44b40c56f52e794837c
theMladyPan <stanislav@rubint.sk>
2024-05-24 17:02:43 +0200

    output format refactor

--- utils/test_high_speed.py
+++ utils/test_high_speed.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import logging
 import argparse
 from serial import Serial
 import struct
 
+log = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Test high speed")
+    """python test_high_speed.py -b 230400|pv>>log.json"""
+    parser = argparse.ArgumentParser(
+        description="Test high speed",
+        epilog="Example: python test_high_speed.py -b 230400|pv>>log.json",
+    )
+
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="verbose mode"
     )
@@ -35,27 +43,43 @@ if __name__ == "__main__":
         # wait for sync packet
         # sync packet is <SOH> <STX> <timestamp:8> <ETX> <EOT>
         while com.read(1) != b"\x01" or com.read(1) != b"\x02":
-            pass
+            log.info("Waiting for sync packet")
         timestamp = com.read(8)
-        timestamp = struct.unpack("<Q", timestamp)[0]
+        try:
+            timestamp = struct.unpack("<Q", timestamp)[0]
+        except struct.error:
+            continue
         etxeot = com.read(2)
         if etxeot != b"\x03\x04":
-            logging.debug("Not a sync packet")
+            log.warning("Not a sync packet")
             continue
 
         data = []
         for _ in range(args.num):
             sohstx = com.read(2)
             if sohstx != b"\x01\x02":
-                logging.warning("Not a valid header")
+                log.warning("Not a valid header")
                 break
             payload = com.read(16)
             eotetx = com.read(2)
             if eotetx != b"\x03\x04":
-                logging.warning(f"Not a valid footer, got {eotetx}")
+                log.warning(f"Not a valid footer, got {eotetx}")
                 break
             # unpack 4 floats:
-            data = struct.unpack("<ffff", payload)
-            print(timestamp, data)
+            values = list(struct.unpack("<ffff", payload))
+            data.append(values)
+            # print(
+            #    f"{int(timestamp/1000):d}, {i}, {data[0]:.5f}, {data[1]:.5f}, {data[2]:.5f}, {data[3]:.5f}"
+            # )
 
-    logging.debug("End high speed test")
+        # print in json format
+        print("{")
+        for i, values in enumerate(data):
+            print(f'  "device_{i}":[')
+            for v in values:
+                print(f"    {v:.5f},")
+            print("  ],")
+        print(f'  "timestamp": {int(timestamp/1000):d},')
+        print("}")
+
+    log.debug("End high speed test")
