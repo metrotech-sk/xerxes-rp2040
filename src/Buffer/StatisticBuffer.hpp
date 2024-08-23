591b191a6c317f907914a248a6d7639e775957fb
theMladyPan <stanislav@rubint.sk>
2024-08-23 15:47:13 +0200

    update for newer SDK

--- src/Buffer/StatisticBuffer.hpp
+++ src/Buffer/StatisticBuffer.hpp
@@ -3,160 +3,159 @@
 
 #include "RingBuffer.hpp"
 #include "Utils/Log.h"
+#include <algorithm>
 
 namespace Xerxes
 {
 
-
-template <class T>
-class StatisticBuffer : public RingBuffer<T>
-{
-protected:
-    float min;
-    float max;
-    float mean;
-    float stdDev;
-    float median;
-public:
-    using RingBuffer<T>::RingBuffer; // inherit constructors
-    void updateStatistics();
-
-    const float & getMean();
-    const float & getStdDev();
-    const float & getMax();
-    const float & getMin();
-    const float & getMedian();
-    void getStatistics(T* min, 
-                       T* max, 
-                       T* mean, 
-                       T* stdDev, 
-                       T* median = nullptr);
-};
-
-
-template <class T>
-void StatisticBuffer<T>::getStatistics(T* min, 
-                                       T* max, 
-                                       T* mean, 
-                                       T* stdDev, 
-                                       T* median)
-{
-    if (min != nullptr) {
-        *min = this->min;
-    }
-
-    if (max != nullptr) {
-        *max = this->max;
-    }
-
-    if (mean != nullptr) {
-        *mean = this->mean;
-    }
-
-    if (stdDev != nullptr) {
-        *stdDev = this->stdDev;
-    }
-    
-    if (median != nullptr) {
-        *median = this->median;
-    }
-}
-
-
-template <class T>
-void StatisticBuffer<T>::updateStatistics()
-{
-    mean = 0;
-    min = INFINITY;
-    max = -INFINITY;
-
-    double sumOfElements {0};
-    double sumOfSquaredErrors {0};
-    std::vector<T> sortedBuffer(this->maxCursor);
-
-    for(int i=0; i<this->maxCursor; i++)
+    template <class T>
+    class StatisticBuffer : public RingBuffer<T>
     {
-        T el = this->buffer[i];
-        sumOfElements += el;
+    protected:
+        float min;
+        float max;
+        float mean;
+        float stdDev;
+        float median;
 
-        if(el < min)
+    public:
+        using RingBuffer<T>::RingBuffer; // inherit constructors
+        void updateStatistics();
+
+        const float &getMean();
+        const float &getStdDev();
+        const float &getMax();
+        const float &getMin();
+        const float &getMedian();
+        void getStatistics(T *min,
+                           T *max,
+                           T *mean,
+                           T *stdDev,
+                           T *median = nullptr);
+    };
+
+    template <class T>
+    void StatisticBuffer<T>::getStatistics(T *min,
+                                           T *max,
+                                           T *mean,
+                                           T *stdDev,
+                                           T *median)
+    {
+        if (min != nullptr)
         {
-            min = el;
+            *min = this->min;
         }
 
-        if(el > max)
+        if (max != nullptr)
         {
-            max = el;
+            *max = this->max;
         }
 
-        sortedBuffer[i] = el;
+        if (mean != nullptr)
+        {
+            *mean = this->mean;
+        }
+
+        if (stdDev != nullptr)
+        {
+            *stdDev = this->stdDev;
+        }
+
+        if (median != nullptr)
+        {
+            *median = this->median;
+        }
     }
 
-    mean = sumOfElements / this->maxCursor;
-
-    for(int i=0; i<this->maxCursor; i++)
+    template <class T>
+    void StatisticBuffer<T>::updateStatistics()
     {
-        sumOfSquaredErrors += powf(this->buffer[i] - mean, 2);
+        mean = 0;
+        min = INFINITY;
+        max = -INFINITY;
+
+        double sumOfElements{0};
+        double sumOfSquaredErrors{0};
+        std::vector<T> sortedBuffer(this->maxCursor);
+
+        for (int i = 0; i < this->maxCursor; i++)
+        {
+            T el = this->buffer[i];
+            sumOfElements += el;
+
+            if (el < min)
+            {
+                min = el;
+            }
+
+            if (el > max)
+            {
+                max = el;
+            }
+
+            sortedBuffer[i] = el;
+        }
+
+        mean = sumOfElements / this->maxCursor;
+
+        for (int i = 0; i < this->maxCursor; i++)
+        {
+            sumOfSquaredErrors += powf(this->buffer[i] - mean, 2);
+        }
+
+        stdDev = sqrtf(sumOfSquaredErrors / this->maxCursor);
+
+        // Calculate the median
+        std::sort(sortedBuffer.begin(), sortedBuffer.end());
+        if (this->maxCursor % 2 == 0)
+        {
+            // If the number of elements is even, take the average of the middle two elements
+            median = (sortedBuffer[this->maxCursor / 2 - 1] + sortedBuffer[this->maxCursor / 2]) / 2.0;
+        }
+        else
+        {
+            // If the number of elements is odd, take the middle element
+            median = sortedBuffer[this->maxCursor / 2];
+        }
+
+        // print content of the sorted buffer
+        for (int i = 0; i < this->maxCursor; i++)
+        {
+            xlog_trace("Sorted buffer[" << i << "]: " << sortedBuffer[i]);
+        }
+        xlog_debug("Median: " << median);
     }
 
-    stdDev = sqrtf(sumOfSquaredErrors / this->maxCursor);
-
-    // Calculate the median
-    std::sort(sortedBuffer.begin(), sortedBuffer.end());
-    if (this->maxCursor % 2 == 0)
+    template <class T>
+    const float &StatisticBuffer<T>::getStdDev()
     {
-        // If the number of elements is even, take the average of the middle two elements
-        median = (sortedBuffer[this->maxCursor / 2 - 1] + sortedBuffer[this->maxCursor / 2]) / 2.0;
+        return stdDev;
     }
-    else
+
+    template <class T>
+    const float &StatisticBuffer<T>::getMean()
     {
-        // If the number of elements is odd, take the middle element
-        median = sortedBuffer[this->maxCursor / 2];
+        return mean;
     }
-    
-    // print content of the sorted buffer
-    for (int i = 0; i < this->maxCursor; i++)
+
+    template <class T>
+    const float &StatisticBuffer<T>::getMin()
     {
-        xlog_trace("Sorted buffer[" << i << "]: " << sortedBuffer[i]);
+        return this->min;
     }
-    xlog_debug("Median: " << median);
-}
 
+    template <class T>
+    const float &StatisticBuffer<T>::getMax()
+    {
+        return max;
+    }
 
-template <class T>
-const float & StatisticBuffer<T>::getStdDev()
-{
-    return stdDev;
-}
+    template <class T>
+    const float &StatisticBuffer<T>::getMedian()
+    {
+        return median;
+    }
 
-
-template <class T>
-const float & StatisticBuffer<T>::getMean()
-{
-    return mean;
-}
-
-
-template <class T>
-const float & StatisticBuffer<T>::getMin()
-{
-    return this->min;
-}
-
-
-template <class T>
-const float & StatisticBuffer<T>::getMax()
-{
-    return max;
-}
-
-template <class T>
-const float & StatisticBuffer<T>::getMedian()
-{
-    return median;
-}
-
-
-} //namespace Xerxes
+} // namespace Xerxes
 
 #endif // !STATISTIC_BUFFER_HPP
